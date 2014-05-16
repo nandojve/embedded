cf6bf909f32154608abc9aeb2d0546e97867b197
Gerson Fernando Budke <nandojve@gmail.com>
2014-05-16 19:31:45 -0300

    Test compile for KITRFA1

--- application/WSNDemo/config/config.h
+++ application/WSNDemo/config/config.h
@@ -45,7 +45,7 @@
 #define _CONFIG_H_
 
 /*- Definitions ------------------------------------------------------------*/
-#define APP_ADDR											0x8001
+#define APP_ADDR											0x0000
 #define APP_PANID											0x1234
 #define APP_SENDING_INTERVAL								2000
 #define APP_ENDPOINT										1
